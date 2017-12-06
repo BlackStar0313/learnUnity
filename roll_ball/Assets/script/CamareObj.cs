4a8a70cc407639132722f5bc17b934d13cdd8d43
zhiwei.liu <270274978@qq.com>
2017-12-06 22:02:52 +0800

    update

---
+++ roll_ball/Assets/script/CamareObj.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamareObj : MonoBehaviour {
+	public GameObject mPlayer ; 
+	private Vector3 mOffset; 
+
+	// Use this for initialization
+	void Start () {
+		this.mOffset = this.transform.position - mPlayer.transform.position; 
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		this.transform.position = this.mOffset + mPlayer.transform.position; 
+	}
+}
---
+++ roll_ball/Assets/script/playerMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+	public float mSpeed = 0 ;
+	public GUIText mTextCoin; 
+	public GUIText mTextWin; 
+	private int mCoinNum ;
+	private Rigidbody mRb ; 
+
+	// Use this for initialization
+	void Start () {
+		this.mRb = GetComponent<Rigidbody> ();
+		this.mCoinNum = 0 ; 
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 moveVec = new Vector3 (moveHorizontal , 0.0f , moveVertical);
+		this.mRb.AddForce (moveVec * this.mSpeed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("pickUpCoin")) {
+			other.gameObject.SetActive(false) ;
+			this.updateTextCoin();
+		}
+	}
+
+	void updateTextCoin() {
+		++this.mCoinNum;
+		this.mTextCoin.text = "coins: " + this.mCoinNum ; 
+		if (this.mCoinNum >= 9 ) {
+			this.mTextWin.gameObject.SetActive(true);
+		} 
+	}
+
+	
+}
